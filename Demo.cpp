1e59c433fb524af2cd70154fd8eec9c98189f38a
webnet <muhommadraahim1@gmail.com>
2018-07-05 10:34:08 +0500

    Add files via upload

--- Demo.cpp
+++ Demo.cpp
@@ -271,9 +271,16 @@ if(choice==7)
     system("del /s /q \ C:\\Users\\%username%\\AppData\\Local\\Temp\\");
     system("del /s /q \ C:\\Windows\\Temp");
     system("del /s /q \ C:\\Temp");
-    Sleep(4000);
+    system("del /s /q \ \"C:\\Users\\%username%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\"");
+    system("del /s /q \ C:\\Users\\%username%\\AppData\\Roaming\\Microsoft\\Windows\\Recent");
+    system("cls");
     cout << "Scan Completed...\n";
     Sleep(4000);
+    system("cls");
+    Sleep(3000);
+    system("color a");
+    cout << "Your system is Healthy";
+    Sleep(3000);
     system("cls");
 }
 
@@ -288,13 +295,22 @@ if(choice==8)
     system("dir f: /a /s");
     system("dir g: /a /s");
     system("dir h: /a /s");
+    system("cls");
+    cout << "Scanning for viruses";
+    Sleep(4000);
     system("del /s /q \ C:\\Users\\%username%\\AppData\\Local\\Temp\\");
     system("del /s /q \ C:\\Windows\\Temp");
     system("del /s /q \ C:\\Temp");
-    //system("del /s /q \ C:\\Users\\%username%\\AppData\\Roaming\\Microsoft\\Windows\\Recent_Items\\");
-    Sleep(4000);
+    system("del /s /q \ \"C:\\Users\\%username%\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\"");
+    system("del /s /q \ C:\\Users\\%username%\\AppData\\Roaming\\Microsoft\\Windows\\Recent");
+    system("cls");
     cout << "Full Scan Completed...\n";
     Sleep(4000);
+    system("cls");
+    Sleep(3000);
+    system("color a");
+    cout << "Your system is Healthy";
+    Sleep(3000);
     system("cls");
 }
 
